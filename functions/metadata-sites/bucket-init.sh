630e7a79eeb094b201991f67c19d5843e9bf00c4
CinthiaVeronicaLopez <cinthiaveronicalopez@gmail.com>
2023-11-02 16:36:25 -0300

    estandarizar nombres de archivos

--- functions/metadata-sites/bucket-init.sh
+++
@@ -1,5 +0,0 @@
-# CREAR BUCKET REGIONAL
-REGION=us-east5
-YOUR_BUCKET_NAME=metadata-sites-us-east5
-
-gsutil mb -l $REGION gs://$YOUR_BUCKET_NAME
--- functions/metadata-sites/cloud-event.sh
+++
@@ -1,4 +0,0 @@
-REGION=us-east5
-YOUR_FUNCTION_NAME=metadata-sites-finalize
-
-gcloud beta functions logs read $YOUR_FUNCTION_NAME --gen2 --region=$REGION --limit=100
